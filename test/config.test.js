41387fa5c330c63c768012bb3cb6facb42f1ca5a
Unknown <huyht@blockchainlabs.asia>
2018-08-24 16:15:50 +0700

    eth wallet

--- test/config.test.js
+++ test/config.test.js
@@ -2,6 +2,6 @@ module.exports = {
   LOG_LEVEL: 'NONE', // ALL, DEBUG, INFO, WARN, ERROR, NONE
   API_KEY: 'test',
   SECRECT: '123456',
-  BASE_URL: 'https://dev-api-testnet.infinitowallet.io',
+  BASE_URL: 'https://staging-api-mainnet.infinitowallet.io',
   AUTH_URL: 'https://dev-api-testnet.infinitowallet.io/v1/auth/token',
 };
