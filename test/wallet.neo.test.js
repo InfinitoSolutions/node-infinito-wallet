e7bee8f3aa0a7709d0df2b847ce7545afb69a3f4
Unknown <huyht@blockchainlabs.asia>
2018-08-30 09:13:27 +0700

    NO smart contract

--- test/wallet.neo.test.js
+++ test/wallet.neo.test.js
@@ -74,7 +74,7 @@ describe('wallet.neo', async () => {
 
   describe('#transfer()', async () => {
 
-    it.only('transfer', async () => {
+    it('transfer', async () => {
       let result = await wallet.transfer('0x9d539c8534c156d76828992fd55a16f79afa9a36', 'APtUVHSAEchsCd6HPrmWXKAK7SETxhAvjU', 1);
       console.log('result transfer NEO: ' + JSON.stringify(result));
       Assert.ok(result.tx_id !== undefined, 'tx id must be exist');
