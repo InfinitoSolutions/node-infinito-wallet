76de2cc2e959b24642c49e58c06a86c32c730413
Cuong Nguyen <cuongnp@cardano-labo.com>
2019-01-25 18:17:42 +0700

    config eslint, install npm package

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 const data = require('../index.js'); 
 global.EthWallet = data.EthWallet;
 global.InfinitoApi = data.InfinitoApi;
-global.Wallet =data.Wallet;
+global.Wallet = data.Wallet;
 global.Account = data.Account;
